8c2c558d53930609e05dd7a0c55ce081cb46b53f
Bilaal Latif <bilaallatif@outlook.com>
2023-08-15 22:46:45 +0100

    Done 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    long_str = '';
+    for (let i = 0; i < num; i++) {
+        long_str += str;
+    }
+    return long_str;
 };
 
 // Do not edit below this line
