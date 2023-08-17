25afe505640a7ec086edbd5ce293fcac40eee87b
Bilaal Latif <bilaallatif@outlook.com>
2023-08-17 01:49:58 +0100

    Done 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    if(num == 0) return 0;
 
+    let sum_prev_prev = 0;
+    let sum_prev = 1;
+
+    for(let i = 2; i <= num; i++){
+        let sum = sum_prev_prev + sum_prev;
+        sum_prev_prev = sum_prev;
+        sum_prev = sum;
+    }
+    return sum_prev;
 };
 
 // Do not edit below this line
