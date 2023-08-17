5eb3e1adc5cfc1b5ce495496fe7e1b456d3ab8b4
Bilaal Latif <bilaallatif@outlook.com>
2023-08-17 01:31:54 +0100

    Done 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    return str.toLowerCase().replace(/[^a-z0-9]/g, '') === str.toLowerCase().replace(/[^a-z0-9]/g, '').split('').reverse().join('');
 };
 
 // Do not edit below this line
