460d3518ce782ada40609d3eb2c154ea86699d1e
Bilaal Latif <bilaallatif@outlook.com>
2023-08-17 02:03:49 +0100

    Done 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let age_arr = arr.map((person) => person.yearOfDeath - person.yearOfBirth);
+    let oldest_age = Math.max(...age_arr);
+    let oldest_person = arr.find((person) => person.yearOfDeath - person.yearOfBirth == oldest_age);
+    return oldest_person;
 };
 
 // Do not edit below this line
